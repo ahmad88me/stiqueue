505659e041adf6a746a6110d5708a49515bbcf69
Ahmad Alobaid <ahmad88me@gmail.com>
2021-09-06 15:13:15 +0200

    add coverage

--- tests/test_client.py
+++ tests/test_client.py
@@ -6,7 +6,6 @@ from stiqueue.sqserver import SQServer
 from stiqueue.sqclient import SQClient
 import os
 import time
-import sys
 
 
 class ClientTest(unittest.TestCase):
@@ -25,9 +24,6 @@ class ClientTest(unittest.TestCase):
         p = multiprocessing.Process(target=cls.start_server, args=(host, port))
         p.start()
         cls.server_process = p
-        # t = threading.Thread(target=cls.start_server)
-        # t.start()
-        # cls.server_thread = t
         time.sleep(1)
         cls.client = SQClient(host=cls.host, port=cls.port)
 
@@ -36,25 +32,9 @@ class ClientTest(unittest.TestCase):
         print("closing things down")
         cls.client.disconnect()
         cls.server_process.terminate()
-        # time.sleep(1)
-        # sys.exit(0)
-        # os._exit(0)
-        # cls.server_thread
-    # def tearDown(self):
-    #     print("closing things down")
-    #     self.client.disconnect()
-    #     time.sleep(1)
-    #     sys.exit(0)
-    #     # os._exit(0)
 
     @classmethod
     def start_server(cls, host, port):
-        # host = None
-        # port = 1234
-        # if 'sqhost' in os.environ:
-        #     host = os.environ['sqhost']
-        # if 'sqport' in os.environ:
-        #     port = int(os.environ['sqport'])
         s = SQServer(host=host, port=port)
         s.listen()
 
@@ -68,6 +48,4 @@ class ClientTest(unittest.TestCase):
         self.assertEqual(a, b"A")
         self.assertEqual(b, b"B")
         self.assertEqual(empty, b'')
-        # self.server_process.terminate()
-        # self.server_thread._stop
-        # sys.exit()
+
